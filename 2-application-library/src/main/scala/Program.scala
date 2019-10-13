ff4406cc746ba87a83aaa996a4b0bd360292d952
martinphee <martyphee@gmail.com>
2019-10-13 11:08:19 +0100

    Base program running

--- 2-application-library/src/main/scala/Program.scala
+++ 2-application-library/src/main/scala/Program.scala
@@ -1,9 +1,55 @@
 object Program {
   def run(args: Array[String]): Unit = {
-    println("-" * 50)
+    display(hyphens)
 
-    println("hello world")
+    display(question)
 
-    println("-" * 50)
+    val input: String = prompt()
+    val integerAmount: Int = convertStringToInt(input)
+    val positiveAmount: Int = ensureAmountIsPositive(integerAmount)
+    val balance: Int = round(positiveAmount)
+    val message: String = createMesssage(balance)
+
+    display(message)
+
+    display(hyphens)
   }
+
+  private val hyphens: String =
+    "\u2500" * 50
+
+  private val question: String =
+    "How much money would you like to deposit"
+
+  // side effect (writing to console)
+  private def display(input: String): Unit = {
+    println(input)
+  }
+
+  // side effecct (reading from the console
+  private def prompt(): String =
+    scala.io.StdIn.readLine
+
+  // potential side effect (throwing of a NumberFormatException
+  private def convertStringToInt(input: String): Int =
+    input.toInt
+
+  private def ensureAmountIsPositive(amount: Int): Int =
+    if(amount < 1)
+      1
+    else
+      amount
+
+  @scala.annotation.tailrec
+  private def round(amount: Int): Int =
+    if(isDivisibleByHundred(amount))
+      amount
+    else
+      round(amount + 1)
+
+  private def isDivisibleByHundred(amount: Int): Boolean =
+    amount % 100 == 0
+
+  private def createMesssage(balance: Int): String =
+    s"Congratulations, you now have USD $balance"
 }
