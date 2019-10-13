7a43a3c32d08e4bde457b1283cb69078945c9aab
martinphee <martyphee@gmail.com>
2019-10-13 13:09:52 +0100

    Cleanup

--- 1-fp-library/src/main/scala/PointFree.scala
+++ 1-fp-library/src/main/scala/PointFree.scala
@@ -2,26 +2,13 @@ package fplibrary
 
 // Constraints Liberate, Liberties Constraint Runar B
 object PointFree {
-  // val ac = compose(ab, bc)
   def compose[A, B, C](ab: A => B, bc: B => C): A => C = a => {
-    // produce a C over here
     val b = ab(a)
     val c = bc(b)
     c
   }
 
-//  def composeKleisli[A, B, C](adb: A => Description[B], bdc: B => Description[C]): A => Description[C] = a => {
-//    // we need to product a Description[C]
-//    val db = adb(a)
-//    val b = db.apply()
-//
-//    val dc = bdc(b)
-//
-//    dc
-//  }
-
   def composeKleisli[A, B, C, D[_]: Monad](adb: A => D[B], bdc: B => D[C]): A => D[C] = a => {
-    // we need to product a Description[C]
     val db = adb(a)
     val dc = Monad[D].flatMap(db)(bdc)
     dc
