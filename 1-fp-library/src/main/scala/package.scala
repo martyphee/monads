a2e97dfef61e35a0614dfe2daf57ce0020e11ede
martinphee <martyphee@gmail.com>
2019-10-13 11:30:29 +0100

    Point free program start

--- 1-fp-library/src/main/scala/package.scala
+++ 1-fp-library/src/main/scala/package.scala
@@ -2,4 +2,6 @@ package object fplibrary {
   private type Thunk[A] = () => A
   type Description[A] = Thunk[A]
 
+  private type RegularArrow[A, B      ] = A => B
+  private type KleisliArrow[A, B, C[_]] = A => C[B]
 }
---
+++ 2-application-library/src/main/scala/PointFreeProgram.scala
@@ -0,0 +1,57 @@
+import fplibrary._
+
+object PointFreeProgram {
+  def createDescription(args: Array[String]): Description[Unit] = Description.create {
+    display(hyphens)
+
+    display(question)
+
+    val input: String = prompt()
+    val integerAmount: Int = convertStringToInt(input)
+    val positiveAmount: Int = ensureAmountIsPositive(integerAmount)
+    val balance: Int = round(positiveAmount)
+    val message: String = createMesssage(balance)
+
+    display(message)
+
+    display(hyphens)
+  }
+
+  private val hyphens: String =
+    "\u2500" * 50
+
+  private val question: String =
+    "How much money would you like to deposit"
+
+  // side effect (writing to console)
+  private def display(input: String): Unit = {
+    println(input)
+  }
+
+  // side effecct (reading from the console
+  private def prompt(): String = "5"
+  //    scala.io.StdIn.readLine
+
+  // potential side effect (throwing of a NumberFormatException
+  private def convertStringToInt(input: String): Int =
+    input.toInt
+
+  private def ensureAmountIsPositive(amount: Int): Int =
+    if(amount < 1)
+      1
+    else
+      amount
+
+  @scala.annotation.tailrec
+  private def round(amount: Int): Int =
+    if(isDivisibleByHundred(amount))
+      amount
+    else
+      round(amount + 1)
+
+  private def isDivisibleByHundred(amount: Int): Boolean =
+    amount % 100 == 0
+
+  private def createMesssage(balance: Int): String =
+    s"Congratulations, you now have USD $balance"
+}
